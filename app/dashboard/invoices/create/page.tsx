f28efc8c1f774ca5fd43cd0a3faae5ea28c54a63
Jay Patel <pjay9981@gmail.com>
2025-01-29 21:21:16 +0530

    Fetch customers data and implement create invoice form with breadcrumbs

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-  return <p>Create invoice</p>;
+import { fetchCustomers } from "@/app/lib/data";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import Form from "@/app/ui/invoices/create-form";
+
+export default async function Page() {
+  const customers = await fetchCustomers();
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          {
+            label: "Invoices",
+            href: "/dashboard/invoices",
+          },
+          {
+            label: "Create Invoice",
+            href: "/dashboard/invoices/create",
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
+  );
 }
